d0231c6eb9ae26171b9163580e439bbc91f7a2cc
Tekku <tekkuuu@protonmail.com>
2025-10-27 15:36:59 +0100

    Tilemap

---
+++ Assets/_Game/Scripts/Grid/Grid.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Grid : MonoBehaviour
+{
+    // Tilemap
+    [SerializeField] private Tilemap _tilemap;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        
+    }
+}
