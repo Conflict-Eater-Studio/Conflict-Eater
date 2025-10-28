e760965da2540ead061ca815f4a4c57a7b722de2
Damian Jasek <damiannq1337@gmail.com>
2025-10-28 22:08:08 +0100

    Add sliders to indicate time left instead clocks

--- Assets/Scripts/UI/TimerUI.cs
+++ Assets/Scripts/UI/TimerUI.cs
@@ -2,6 +2,7 @@ using System;
 using System.Reflection;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public static class Extensions {
     public static void SetText(this TMP_Text text, string value) {
@@ -12,11 +13,13 @@ public class TimerUI : MonoBehaviour
 {
     [SerializeField] TMP_Text _matchTimeLeftText;
     [SerializeField] TMP_Text _roundTimeLeftText;
-
+    [SerializeField] Slider _matchSlider;
+    [SerializeField] Slider _roundSlider;
+    
     private float _matchStartTime; 
     private float _roundStartTime;
-    private float _matchDurationInMinutes;
-    private float _roundDurationInMinutes;
+    private float _matchDurationSeconds;
+    private float _roundDurationSeconds;
     private void Awake() {
         GameManager.Instance.Timer.OnMatchStart += TimerUI_OnMatchStart;
         GameManager.Instance.Timer.OnRoundEnd += (sender, args) => {
@@ -24,17 +27,30 @@ public class TimerUI : MonoBehaviour
         };
     }
     private void Update() {
-        TimeSpan matchTimeLeft = TimeSpan.FromSeconds((_matchStartTime + _matchDurationInMinutes) - Time.time);
-        TimeSpan roundTimeLeft = TimeSpan.FromSeconds((_roundStartTime + _roundDurationInMinutes) - Time.time);
+        float roundTimeLeft = (_roundStartTime + _roundDurationSeconds) - Time.time;
+        float roundTimeLeftPercent = roundTimeLeft / _roundDurationSeconds;
+        float matchTimeLeft = (_matchStartTime + _matchDurationSeconds) - Time.time;
+        float matchTimeLeftPercent = matchTimeLeft / _matchDurationSeconds;
+        if(matchTimeLeft <= 0) return;
+        if(roundTimeLeft <= 0) return;
+        _roundSlider.value = roundTimeLeftPercent;
+        _matchSlider.value = matchTimeLeftPercent;
         
+        UpdateTimerDisplay();
+    }
+    private void UpdateTimerDisplay() {
+        TimeSpan matchTimeLeft = TimeSpan.FromSeconds((_matchStartTime + _matchDurationSeconds) - Time.time);
+        TimeSpan roundTimeLeft = TimeSpan.FromSeconds((_roundStartTime + _roundDurationSeconds) - Time.time);
+        if(matchTimeLeft.Seconds < 0 || roundTimeLeft.Seconds < 0) return;
         _matchTimeLeftText.SetText($"{matchTimeLeft.Minutes:00}:{matchTimeLeft.Seconds:00}");
-        _roundTimeLeftText.SetText($"{roundTimeLeft.Minutes:00}:{roundTimeLeft.Seconds:00}");
+        _roundTimeLeftText.SetText($"{roundTimeLeft.TotalMinutes:00}:{roundTimeLeft.TotalSeconds:00}");
     }
 
     private void TimerUI_OnMatchStart(object sender, OnMatchStartEventArgs e) {
         _matchStartTime = e.MatchStartTime;
         _roundStartTime = e.MatchStartTime;
-        _matchDurationInMinutes = e.MatchDuration;
-        _roundDurationInMinutes = e.RoundDuration;
+        _matchDurationSeconds = e.MatchDuration;
+        _roundDurationSeconds = e.RoundDuration;
+        UpdateTimerDisplay();
     }
 }
