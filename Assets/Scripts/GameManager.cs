196fefe37dbedc5d87a3bb453d2ba1e206b05020
Damian Jasek <damiannq1337@gmail.com>
2025-10-29 20:04:30 +0100

    Fix script execution order

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,18 +6,8 @@ public enum GameState {
     Running
 }
 public class GameManager : Singleton<GameManager> {
-
     [SerializeField] private GameState _gameState = GameState.Running;
     [SerializeField] public Match Timer;
     [SerializeField] public RoleSwapper RoleSwapper;
-    private void Awake() {
-        if (Timer == null) {
-            if (TryGetComponent(typeof(Match), out Component compenent)) {
-                Timer = compenent as Match;
-            }
-            else {
-                Timer = FindAnyObjectByType<Match>();
-            }
-        }
-    }
+
 }
--- Assets/Scripts/UI/MenuUI.cs
+++ Assets/Scripts/UI/MenuUI.cs
@@ -5,13 +5,20 @@ using UnityEngine.UI;
 public class MenuUI : MonoBehaviour
 {
     [SerializeField] private Button _startButton;
+    [SerializeField] private Button _pauseButton;
+    [SerializeField] private Button _resumeButton;
+    [SerializeField] private Button _endRoundButton;
     private void OnEnable() {
-        _startButton.onClick.AddListener(TaskOnClick);
+        _startButton.onClick.AddListener(GameManager.Instance.Timer.Run);
+        _pauseButton.onClick.AddListener(GameManager.Instance.Timer.Pause);
+        _resumeButton.onClick.AddListener(GameManager.Instance.Timer.Resume);
+        _endRoundButton.onClick.AddListener(GameManager.Instance.Timer.EndRound);       
     }
     private void OnDisable() {
-        _startButton.onClick.RemoveListener(TaskOnClick);
+        _startButton.onClick.RemoveListener(GameManager.Instance.Timer.Run);
+        _pauseButton.onClick.RemoveListener(GameManager.Instance.Timer.Pause);
+        _resumeButton.onClick.RemoveListener(GameManager.Instance.Timer.Resume);
+        _endRoundButton.onClick.RemoveListener(GameManager.Instance.Timer.EndRound);       
     }
-    void TaskOnClick() {
-        GameManager.Instance.Timer.Run();   
-    }
+ 
 }
