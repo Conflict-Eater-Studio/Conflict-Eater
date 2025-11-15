34b5006016016a8a90b38952226cb6b670c80ead
Damian Jasek <damiannq1337@gmail.com>
2025-11-15 17:12:15 +0100

    Fix powerup spawning

--- Assets/Scripts/PowerupSpawner.cs
+++ Assets/Scripts/PowerupSpawner.cs
@@ -2,22 +2,23 @@ using System;
 using UnityEngine;
 using Random = System.Random;
 
-public class PowerupSpawner : MonoBehaviour
-{
+public class PowerupSpawner : MonoBehaviour {
     [Tooltip("Number of powerups to spawn in game")]
-    [SerializeField] private float _minimumRoundTimePassed = 10f;
-    [SerializeField] private float _maximumRoundTimePassed = 20f;
+    [SerializeField] private Vector2 _spawnWindow;
 
     [SerializeField] private int _powerupCount = 1;
     [SerializeField] private GameObject _powerupPrefab;
     [SerializeField] Grid _grid;
     void Start() {
         GameManager.Instance.Timer.OnRoundStart += PowerupSpawner_OnRoundStart;
+        GameManager.Instance.Timer.OnRoundEnd += PowerupSpawner_OnRoundEnd;
+    }
+    private void PowerupSpawner_OnRoundEnd(object sender, EventArgs e) {
+            CancelInvoke(nameof(SpawnPowerup));
     }
     private void PowerupSpawner_OnRoundStart(object sender, EventArgs e) {
-           float randomTime = UnityEngine.Random.Range(_minimumRoundTimePassed, _maximumRoundTimePassed);
-            Debug.Log(randomTime);
-           Invoke("SpawnPowerup", randomTime);
+           float randomTime = UnityEngine.Random.Range(_spawnWindow.x, _spawnWindow.y);
+           Invoke(nameof(SpawnPowerup), randomTime);
     }
     private void SpawnPowerup() {
         Vector2 spawnPoint = _grid.GetSpawnPoint(Grid.SpawnPointType.PowerUpRandom);
--- Assets/_Game/Scripts/Powerup.cs
+++ Assets/_Game/Scripts/Powerup.cs
@@ -6,11 +6,13 @@ public class Powerup : MonoBehaviour
         if (other.gameObject.CompareTag("PlayerLight")) {
             Debug.Log("Light Powerup collected!");
             GameManager.Instance.IsFrightenedShadowState = true;
+            Destroy(this.gameObject);
         }
         if (other.TryGetComponent(typeof(ShadowController), out Component component)) {
             Debug.Log("Shadow Powerup collected!");
             ShadowController _controller = (ShadowController) component;
             _controller.Movement.SpeedMult = 10f;
+            Destroy(this.gameObject);
         }
         
     }
