0885673d0d5cba6e5a2de450c361de9552f84d8b
Damian Jasek <damiannq1337@gmail.com>
2025-11-15 16:41:03 +0100

    Add spawn window for powerups

--- Assets/Scripts/PowerupSpawner.cs
+++ Assets/Scripts/PowerupSpawner.cs
@@ -1,13 +1,23 @@
+using System;
 using UnityEngine;
+using Random = System.Random;
 
 public class PowerupSpawner : MonoBehaviour
 {
     [Tooltip("Number of powerups to spawn in game")]
+    [SerializeField] private float _minimumRoundTimePassed = 10f;
+    [SerializeField] private float _maximumRoundTimePassed = 20f;
+
     [SerializeField] private int _powerupCount = 1;
     [SerializeField] private GameObject _powerupPrefab;
     [SerializeField] Grid _grid;
     void Start() {
-        SpawnPowerup();
+        GameManager.Instance.Timer.OnRoundStart += PowerupSpawner_OnRoundStart;
+    }
+    private void PowerupSpawner_OnRoundStart(object sender, EventArgs e) {
+           float randomTime = UnityEngine.Random.Range(_minimumRoundTimePassed, _maximumRoundTimePassed);
+            Debug.Log(randomTime);
+           Invoke("SpawnPowerup", randomTime);
     }
     private void SpawnPowerup() {
         Vector2 spawnPoint = _grid.GetSpawnPoint(Grid.SpawnPointType.PowerUpRandom);
--- Assets/_Game/Scripts/Powerup.cs
+++ Assets/_Game/Scripts/Powerup.cs
@@ -5,10 +5,11 @@ public class Powerup : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.CompareTag("PlayerLight")) {
             Debug.Log("Light Powerup collected!");
+            GameManager.Instance.IsFrightenedShadowState = true;
         }
         if (other.TryGetComponent(typeof(ShadowController), out Component component)) {
+            Debug.Log("Shadow Powerup collected!");
             ShadowController _controller = (ShadowController) component;
-            if(!_controller.IsActive) return;
             _controller.Movement.SpeedMult = 10f;
         }
         
